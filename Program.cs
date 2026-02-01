ac9f5edd5fd0c70658226224c13019a2d78d3624
Xdivanovich <ivanpousad@gmail.com>
2026-02-01 11:41:52 +0100

    Refactor OpenAPI setup and add root endpoint
    Updated OpenAPI configuration and added a new endpoint.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi(); 
+
+app.MapGet("/", () => "¡El Backend está funcionando correctamente en Azure!");
 
 app.UseHttpsRedirection();
 
